a173ad0618204b9a0e8cd44d5110d7a5e94f2d48
iSketch_up <kalulu@gmx.at>
2026-06-23 12:45:29 +0200

    stuff

--- italy/scenes/game.gd
+++ italy/scenes/game.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var trash_scene: PackedScene = preload("res://scenes/trash.tscn")
 
-@export var spawn_area := Rect2(Vector2(00, 100), Vector2(00, 300))
+@export var spawn_area := Rect2(Vector2(200, 100), Vector2(500, 300))
 
 @export var min_spawn_time := 0.5
 @export var max_spawn_time := 1.5
@@ -13,9 +13,14 @@ extends Node2D
 var rng := RandomNumberGenerator.new()
 var spawn_timer := 0.0
 
+var score := 0
+
+@onready var score_label: Label = $ScoreLabel
+
 
 func _ready() -> void:
 	rng.randomize()
+	_update_score_label()
 	_reset_spawn_timer()
 
 
@@ -28,7 +33,7 @@ func _process(delta: float) -> void:
 
 
 func _spawn_trash() -> void:
-	var trash := trash_scene.instantiate()
+	var trash := trash_scene.instantiate() as Trash
 	add_child(trash)
 
 	trash.position = Vector2(
@@ -36,13 +41,22 @@ func _spawn_trash() -> void:
 		rng.randf_range(spawn_area.position.y, spawn_area.end.y)
 	)
 
+	trash.scored.connect(_on_trash_scored)
+
 	var direction := 1
 	
 
 	var speed := rng.randf_range(min_speed, max_speed)
+	trash.setup(direction, speed)
 
-	if trash.has_method("setup"):
-		trash.setup(direction, speed)
+
+func _on_trash_scored(points: int) -> void:
+	score += points
+	_update_score_label()
+
+
+func _update_score_label() -> void:
+	score_label.text = "Score: %d" % score
 
 
 func _reset_spawn_timer() -> void:
